65fb9dc421cfa09adc5a86033cf1ac3ecd6f67dd
remuruTempest <55364136+remuruTempest@users.noreply.github.com>
2025-01-15 03:23:09 -0700

    Image Slider
    Another project in practicing fetch API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Accordian from './components/accordian';
+import ImageSlider from './components/image-slider';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 
@@ -14,7 +15,11 @@ function App() {
      {/*<h1>Random Color component</h1> */}
      {/* <RandomColor/> */}
       {/* Star Rating component */}
-     <StarRating noOfStars={10}/>
+     {/* <StarRating noOfStars={10}/> */}
+     {/* Image slidere */}
+     <ImageSlider url={"https://picsum.photos/v2/list"} 
+             page={"1"}
+             limit={"10"}/>
     </div>
   );
 }
