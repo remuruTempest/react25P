02bb6f542f52adc36246a5b113b82e1fb55f1c7c
remuruTempest <55364136+remuruTempest@users.noreply.github.com>
2025-01-16 22:51:52 -0700

    Load More Data project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Accordian from './components/accordian';
 import ImageSlider from './components/image-slider';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
+import LoadMoreData from './components/load-more-data';
 
 function App() {
   return (
@@ -17,9 +18,12 @@ function App() {
       {/* Star Rating component */}
      {/* <StarRating noOfStars={10}/> */}
      {/* Image slidere */}
-     <ImageSlider url={"https://picsum.photos/v2/list"} 
+     {/* <ImageSlider url={"https://picsum.photos/v2/list"} 
              page={"1"}
-             limit={"10"}/>
+             limit={"10"}/> */}
+      {/* Load More Data */}
+      {/* <LoadMoreData/> */}
+      
     </div>
   );
 }
