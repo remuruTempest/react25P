7c21b24027953d79266162c721a20bd839c07d4e
remuruTempest <55364136+remuruTempest@users.noreply.github.com>
2025-01-25 23:32:10 -0700

    update

--- src/components/custom-modal-popup/modal-test.jsx
+++ src/components/custom-modal-popup/modal-test.jsx
@@ -14,12 +14,21 @@ export default function ModalTest(){
         setshowModalPopup(!showModalPopup)
     };
 
+    function onClose() {
+        setshowModalPopup(false);
+      };
+
     return (
             <div className="container">
                 <button onClick={handleToggleModalPopup}>Open Modal Pop-up</button>
                 {
                     showModalPopup && <Modal
-                    body={<div>Customize Body</div>}
+
+                    id={"custom-id"}
+                    header={<h1>Customized Header</h1>}
+                    footer={<h1>Customized Footer</h1>}
+                    onClose={onClose}
+                    body={<div>Customized body</div>}
                     
                     />
                 }
